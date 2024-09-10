16653e6171a6e75a0668daaa8854119f78045d44
Rebekah Kozinski <bekahkozinski@gmail.com>
2024-09-09 20:25:08 -0500

    Class 2 Studio Character Count

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CharacterCountBonus.java
@@ -0,0 +1,30 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterCountBonus {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a string to count its characters: ");
+        String userString = input.nextLine();
+
+        char[] charArray = userString.toUpperCase().toCharArray();
+
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        for (char letter :charArray) {
+            if (counts.containsKey(letter)) {
+                counts.put(letter, counts.get(letter) + 1);
+            } else {
+                counts.put(letter, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> count :counts.entrySet())
+            System.out.println(count.getKey()+": "+count.getValue());
+    }
+}
